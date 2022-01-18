34b38ac68a726d3c4a3bcdd62170370425e5d40d
Moustapha KINTY <moustaphakinty@moustaphak-mac.local>
2022-01-18 21:59:23 +0100

    render pdf file

--- quizzes/utils.py
+++ quizzes/utils.py
@@ -1,68 +1,70 @@
 import random
 
 # To generate PDF white weasyprint
-# from django.conf import settings
-# from django.template.loader import render_to_string
-# from django.shortcuts import HttpResponse
-# import weasyprint
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.shortcuts import HttpResponse
+import weasyprint
 
 # To generate PDF white xhtml2pdf
-from django.conf import settings
-from django.contrib.staticfiles import finders
+# from django.conf import settings
+# from django.contrib.staticfiles import finders
+
+
 # datetime
 from datetime import datetime
 
 
-def link_callback(uri, rel):
-    """
-    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
-    resources
-    """
-    result = finders.find(uri)
-    if result:
-        if not isinstance(result, (list, tuple)):
-            result = [result]
-        result = list(os.path.realpath(path) for path in result)
-        path = result[0]
-    else:
-        sUrl = settings.STATIC_URL  # Typically /static/
-        sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
-        mUrl = settings.MEDIA_URL  # Typically /media/
-        mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
-
-        if uri.startswith(mUrl):
-            path = os.path.join(mRoot, uri.replace(mUrl, ""))
-        elif uri.startswith(sUrl):
-            path = os.path.join(sRoot, uri.replace(sUrl, ""))
-        else:
-            return uri
-
-    # make sure that file exists
-    if not os.path.isfile(path):
-        raise Exception(
-            'media URI must start with %s or %s' % (sUrl, mUrl)
-        )
-    return path
+# def link_callback(uri, rel):
+#     """
+#     Convert HTML URIs to absolute system paths so xhtml2pdf can access those
+#     resources
+#     """
+#     result = finders.find(uri)
+#     if result:
+#         if not isinstance(result, (list, tuple)):
+#             result = [result]
+#         result = list(os.path.realpath(path) for path in result)
+#         path = result[0]
+#     else:
+#         sUrl = settings.STATIC_URL  # Typically /static/
+#         sRoot = settings.STATIC_ROOT  # Typically /home/userX/project_static/
+#         mUrl = settings.MEDIA_URL  # Typically /media/
+#         mRoot = settings.MEDIA_ROOT  # Typically /home/userX/project_static/media/
+#
+#         if uri.startswith(mUrl):
+#             path = os.path.join(mRoot, uri.replace(mUrl, ""))
+#         elif uri.startswith(sUrl):
+#             path = os.path.join(sRoot, uri.replace(sUrl, ""))
+#         else:
+#             return uri
+#
+#     # make sure that file exists
+#     if not os.path.isfile(path):
+#         raise Exception(
+#             'media URI must start with %s or %s' % (sUrl, mUrl)
+#         )
+#     return path
 
 
 # *-----------------------------------------------------------------------------------------*
 # if weasyprint:
 # *-----------------------------------------------------------------------------------------*
-# def generate_pdf(request, *args):
-#     template_path = args[0]
-#     pdf_name = args[1]
-#     context = args[2]
-#     response = HttpResponse(content_type='application/pdf')
-#     # if download:
-#     response['Content-Disposition'] = f'attachment; filename={pdf_name}_{datetime.now()}.pdf'
-#     # if display:
-#     response['Content-Disposition'] = f'filename={pdf_name}_{datetime.now()}.pdf'
-#     # response['Content-Tranfer-Encoding'] = 'binary'
-#     html_string = render_to_string(template_path, context)
-#     html = weasyprint.HTML(string=html_string, base_url=request.build_absolute_uri())
-#     css = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
-#     html.write_pdf(response, stylesheets=css)
-#     return response
+def generate_pdf(request, *args):
+    template_path = args[0]
+    pdf_name = args[1]
+    context = args[2]
+    response = HttpResponse(content_type='application/pdf')
+    # if download:
+    # response['Content-Disposition'] = f'attachment; filename={pdf_name}_{datetime.now()}.pdf'
+    # if display:
+    response['Content-Disposition'] = f'filename={pdf_name}_{datetime.now()}.pdf'
+    response['Content-Tranfer-Encoding'] = 'binary'
+    html_string = render_to_string(template_path, context)
+    html = weasyprint.HTML(string=html_string, base_url=request.build_absolute_uri())
+    css = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
+    html.write_pdf(response, stylesheets=css)
+    return response
 # *-----------------------------------------------------------------------------------------*
 # end if weasyprint
 # *-----------------------------------------------------------------------------------------*
